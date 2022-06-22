394a07a3787f787eb5a7d4a5d4f873300864f5e1
blkopsdev <blkopsdev@gmail.com>
2022-06-22 07:09:43 -0700

    modified:   resources/views/customer/auth/passwords/confirm.blade.php 	modified:   resources/views/customer/auth/passwords/email.blade.php 	modified:   resources/views/customer/auth/passwords/reset.blade.php

--- resources/views/customer/auth/passwords/email.blade.php
+++ resources/views/customer/auth/passwords/email.blade.php
@@ -1,38 +1,41 @@
 @extends('layouts.app', ['class' => 'off-canvas-sidebar', 'activePage' => 'email', 'title' => __('Recruiting Brain')])
 
 @section('content')
-<div class="container" style="height: auto;">
-  <div class="row align-items-center">
-    <div class="col-lg-4 col-md-6 col-sm-8 ml-auto mr-auto">
-      <form class="form" method="POST" action="{{ route('premium.password.email') }}">
-        @csrf
+<div class="content">
 
-        <div class="card card-login card-hidden mb-3">
-          <div class="card-header card-header-primary text-center">
-            <h4 class="card-title"><strong>{{ __('Forgot Password') }}</strong></h4>
-          </div>
-          <div class="card-body">
-            <div class="bmd-form-group{{ $errors->has('email') ? ' has-danger' : '' }}">
-              <div class="input-group">
-                <div class="input-group-prepend">
-                  <span class="input-group-text">
-                    <i class="material-icons">email</i>
-                  </span>
+  <div class="container" style="height: auto;">
+    <div class="row align-items-center">
+      <div class="col-lg-4 col-md-6 col-sm-8 ml-auto mr-auto">
+        <form class="form" method="POST" action="{{ route('premium.password.email') }}">
+          @csrf
+  
+          <div class="card card-login card-hidden mb-3">
+            <div class="card-header card-header-primary text-center">
+              <h4 class="card-title"><strong>{{ __('Forgot Password') }}</strong></h4>
+            </div>
+            <div class="card-body">
+              <div class="bmd-form-group{{ $errors->has('email') ? ' has-danger' : '' }}">
+                <div class="input-group">
+                  <div class="input-group-prepend">
+                    <span class="input-group-text">
+                      <i class="material-icons">email</i>
+                    </span>
+                  </div>
+                  <input type="email" name="email" class="form-control" placeholder="{{ __('Email...') }}" value="{{ old('email') }}" required>
                 </div>
-                <input type="email" name="email" class="form-control" placeholder="{{ __('Email...') }}" value="{{ old('email') }}" required>
+                @if ($errors->has('email'))
+                  <div id="email-error" class="error text-danger pl-3" for="email" style="display: block;">
+                    <strong>{{ $errors->first('email') }}</strong>
+                  </div>
+                @endif
               </div>
-              @if ($errors->has('email'))
-                <div id="email-error" class="error text-danger pl-3" for="email" style="display: block;">
-                  <strong>{{ $errors->first('email') }}</strong>
-                </div>
-              @endif
+            </div>
+            <div class="card-footer justify-content-center">
+              <button type="submit" class="btn btn-primary btn-link btn-lg">{{ __('Send Password Reset Link') }}</button>
             </div>
           </div>
-          <div class="card-footer justify-content-center">
-            <button type="submit" class="btn btn-primary btn-link btn-lg">{{ __('Send Password Reset Link') }}</button>
-          </div>
-        </div>
-      </form>
+        </form>
+      </div>
     </div>
   </div>
 </div>
--- resources/views/customer/auth/passwords/reset.blade.php
+++ resources/views/customer/auth/passwords/reset.blade.php
@@ -1,55 +1,58 @@
-@extends('layouts.app', ['class' => 'off-canvas-sidebar', 'activePage' => '', 'title' => __('Material Dashboard')])
+@extends('layouts.app', ['class' => 'off-canvas-sidebar', 'activePage' => 'Password Reset', 'title' => __('Material Dashboard')])
 
 @section('content')
-<div class="container" style="height: auto;">
-  <div class="row align-items-center">
-    <div class="col-lg-4 col-md-6 col-sm-8 ml-auto mr-auto">
-      <form class="form" method="POST" action="{{ route('premium.password.update') }}">
-        @csrf
+<div class="content">
 
-        <input type="hidden" name="token" value="{{ $token }}">
-        <input type="hidden" name="email" value="{{ $email }}">
-        <div class="card card-login card-hidden mb-3">
-          <div class="card-header card-header-primary text-center">
-            <h4 class="card-title"><strong>{{ __('Reset Password') }}</strong></h4>
-          </div>
-          <div class="card-body ">
-            <div class="bmd-form-group{{ $errors->has('password') ? ' has-danger' : '' }} mt-3">
-              <div class="input-group">
-                <div class="input-group-prepend">
-                  <span class="input-group-text">
-                    <i class="material-icons">lock_outline</i>
-                  </span>
-                </div>
-                <input type="password" name="password" id="password" class="form-control" placeholder="{{ __('Password...') }}" required>
-              </div>
-              @if ($errors->has('password'))
-                <div id="password-error" class="error text-danger pl-3" for="password" style="display: block;">
-                  <strong>{{ $errors->first('password') }}</strong>
-                </div>
-              @endif
+  <div class="container" style="height: auto;">
+    <div class="row align-items-center">
+      <div class="col-lg-4 col-md-6 col-sm-8 ml-auto mr-auto">
+        <form class="form" method="POST" action="{{ route('premium.password.update') }}">
+          @csrf
+  
+          <input type="hidden" name="token" value="{{ $token }}">
+          <input type="hidden" name="email" value="{{ $email }}">
+          <div class="card card-login card-hidden mb-3">
+            <div class="card-header card-header-primary text-center">
+              <h4 class="card-title"><strong>{{ __('Reset Password') }}</strong></h4>
             </div>
-            <div class="bmd-form-group{{ $errors->has('password_confirmation') ? ' has-danger' : '' }} mt-3">
-              <div class="input-group">
-                <div class="input-group-prepend">
-                  <span class="input-group-text">
-                    <i class="material-icons">lock_outline</i>
-                  </span>
+            <div class="card-body ">
+              <div class="bmd-form-group{{ $errors->has('password') ? ' has-danger' : '' }} mt-3">
+                <div class="input-group">
+                  <div class="input-group-prepend">
+                    <span class="input-group-text">
+                      <i class="material-icons">lock_outline</i>
+                    </span>
+                  </div>
+                  <input type="password" name="password" id="password" class="form-control" placeholder="{{ __('Password...') }}" required>
                 </div>
-                <input type="password" name="password_confirmation" id="password_confirmation" class="form-control" placeholder="{{ __('Confirm Password...') }}" required>
+                @if ($errors->has('password'))
+                  <div id="password-error" class="error text-danger pl-3" for="password" style="display: block;">
+                    <strong>{{ $errors->first('password') }}</strong>
+                  </div>
+                @endif
               </div>
-              @if ($errors->has('password_confirmation'))
-                <div id="password_confirmation-error" class="error text-danger pl-3" for="password_confirmation" style="display: block;">
-                  <strong>{{ $errors->first('password_confirmation') }}</strong>
+              <div class="bmd-form-group{{ $errors->has('password_confirmation') ? ' has-danger' : '' }} mt-3">
+                <div class="input-group">
+                  <div class="input-group-prepend">
+                    <span class="input-group-text">
+                      <i class="material-icons">lock_outline</i>
+                    </span>
+                  </div>
+                  <input type="password" name="password_confirmation" id="password_confirmation" class="form-control" placeholder="{{ __('Confirm Password...') }}" required>
                 </div>
-              @endif
+                @if ($errors->has('password_confirmation'))
+                  <div id="password_confirmation-error" class="error text-danger pl-3" for="password_confirmation" style="display: block;">
+                    <strong>{{ $errors->first('password_confirmation') }}</strong>
+                  </div>
+                @endif
+              </div>
+            </div>
+            <div class="card-footer justify-content-center">
+              <button type="submit" class="btn btn-primary btn-link btn-lg">{{ __('Reset Password') }}</button>
             </div>
           </div>
-          <div class="card-footer justify-content-center">
-            <button type="submit" class="btn btn-primary btn-link btn-lg">{{ __('Reset Password') }}</button>
-          </div>
-        </div>
-      </form>
+        </form>
+      </div>
     </div>
   </div>
 </div>
