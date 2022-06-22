f94ade4fc74daf2b753fb0b2177723717cf32d6c
blkopsdev <blkopsdev@gmail.com>
2022-06-21 23:52:57 -0700

    modified:   resources/views/layouts/footers/auth.blade.php

--- resources/views/layouts/footers/auth.blade.php
+++ resources/views/layouts/footers/auth.blade.php
@@ -2,9 +2,9 @@
   <div class="container-fluid">
     <div class="copyright float-right">
       &copy;
-      <script>
-          document.write(new Date().getFullYear())
-      </script>, Powered by  <strong class="text-primary">Recruiting Brain</strong>
+        <script>
+            document.write(new Date().getFullYear())
+        </script> The Recruiting Brain - All Rights Reserved | Powered By AWS Sports
       
   </div>
 </footer>
